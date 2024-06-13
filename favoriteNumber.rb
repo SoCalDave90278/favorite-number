b28bc1a30b1279212ea3ba1bdc5da59a29055869
SoCalDave90278 <david@digitallifecyclemarketing.com>
2024-06-12 20:25:55 -0700

    Add files via upload
    Initial commit

---
+++ favoriteNumber.rb
@@ -0,0 +1,6 @@
+puts "Hello there, and what is your favorite number?"
+f_number = gets.chomp
+puts "#{f_number} is a very fine number!"
+var1 = f_number
+var2 = 1 + f_number.to_i
+puts "However, #{var2} is an even grander number!"
